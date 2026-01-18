03075375d6a4164fb18f919b190221af4c381830
SonalAmarasekera <164004817+SonalAmarasekera@users.noreply.github.com>
2026-01-18 19:33:12 +0800

    Delete fixed directory

--- fixed/gen_latents_fixed.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Generate Latents
-python cache_latents.py --csv train_min.csv --out_dir /workspace/latents/min/train --model_type 16khz --device cuda
-
-python cache_latents.py --csv dev_min.csv --out_dir /workspace/latents/min/dev --model_type 16khz --device cuda
-
-python cache_latents.py --csv test_min.csv --out_dir /workspace/latents/min/test --model_type 16khz --device cuda
